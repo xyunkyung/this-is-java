1e52584cfe73209101cb76b70177e0d1e97a4648
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-04-16 16:15:04 +0900

    Add files via upload

---
+++ 03-q5.java
@@ -0,0 +1,11 @@
+
+public class q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int value = 356;
+		System.out.println(value - (value % 300));
+	}
+
+}
---
+++ 03-q6.java
@@ -0,0 +1,15 @@
+
+public class q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = ((((double)lengthTop + lengthBottom) * height) / 2);
+		// 사다리꼴 넓이 구하는 공식 = (윗변 + 아랫변) * 높이 / 2
+		System.out.println(area);
+	}
+
+}
