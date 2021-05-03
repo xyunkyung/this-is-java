fd6c568d27f87dd291037d06fdcaac5ad2d158ed
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-05-04 08:57:39 +0900

    Add files via upload

---
+++ 10-q07/NotExistIDExcapetion.java
@@ -0,0 +1,9 @@
+package q07;
+
+public class NotExistIDExcapetion extends Exception {
+
+	public NotExistIDExcapetion() {}
+	public NotExistIDExcapetion(String message) {
+		super(message);
+	}
+}
---
+++ 10-q07/WrongPasswordException.java
@@ -0,0 +1,9 @@
+package q07;
+
+public class WrongPasswordException extends Exception {
+
+	public WrongPasswordException() {}
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+}
